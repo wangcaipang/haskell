ab599deee6fdc61139bebb522ac62f776d4380b0
wangcai <wangcai@ali-acbc328ae08b.local>
2017-06-20 19:13:23 +0800

    add

---
+++ real-world-haskell/ch10/PNM.hs
@@ -0,0 +1,22 @@
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Lazy as L
+import Data.Char (isSpace)
+
+data Greymap = Greymap {
+  greyWidth :: Int,
+  greyHeight :: Int,
+  greyMax :: Int,
+  greyData :: L.ByteString
+} deriving (Eq)
+
+instance Show Greymap where
+  show (Greymap w h m _) = "GreyMap " ++ show w ++ "x" ++ show h ++ " " ++ show m
+
+parseP5 :: L.ByteString -> Maybe (Greymap, L.ByteString)
+
+matchHeader :: L.ByteString -> L.ByteString -> Maybe L.ByteString
+
+getNat :: L.ByteString -> Maybe (Int, L.ByteString)
+
+getBytes :: Int -> L.ByteString -> Maybe (L.ByteString, L.ByteString)
+
